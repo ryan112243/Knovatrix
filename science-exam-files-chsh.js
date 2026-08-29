ed74e52c9b49a664a87085f5ccfad733b78ac26f
ryan112243 <ryan112243@users.noreply.github.com>
2026-08-29 20:45:31 +0800

    Provide Changhua science exams as ZIP

--- science-exam-files-chsh.js
+++ science-exam-files-chsh.js
@@ -6,91 +6,91 @@
   "106": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//106//106-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//106//106-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//106//106-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//106//106-實驗實作試題及參考答案.zip"
     }
   ],
   "107": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//107//107-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//107//107-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//107//107-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//107//107-實驗實作試題及參考答案.zip"
     }
   ],
   "108": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//108//108-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//108//108-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//108//108-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//108//108-實驗實作試題及參考答案.zip"
     }
   ],
   "109": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//109//109-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//109//109-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//109//109-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//109//109-實驗實作試題及參考答案.zip"
     }
   ],
   "110": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//110//110-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//110//110-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//110//110-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//110//110-實驗實作試題及參考答案.zip"
     }
   ],
   "111": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//111//111-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//111//111-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//111//111-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//111//111-實驗實作試題及參考答案.zip"
     }
   ],
   "112": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//112//112-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//112//112-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//112//112-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//112//112-實驗實作試題及參考答案.zip"
     }
   ],
   "113": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//113//113-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//113//113-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//113//113-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//113//113-實驗實作試題及參考答案.zip"
     }
   ],
   "114": [
     {
       "label": "科學能力檢定試題及參考答案",
-      "path": "files//science-class//chsh//114//114-科學能力檢定試題及參考答案.rar"
+      "path": "files//science-class//chsh//114//114-科學能力檢定試題及參考答案.zip"
     },
     {
       "label": "實驗實作試題及參考答案",
-      "path": "files//science-class//chsh//114//114-實驗實作試題及參考答案.rar"
+      "path": "files//science-class//chsh//114//114-實驗實作試題及參考答案.zip"
     }
   ]
 };
